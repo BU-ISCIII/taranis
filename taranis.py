3d9debb63a2839a77485d925ea1485d7a0b647ff
luissian <chapado.l@gmail.com>
2019-03-20 16:28:05 +0100

    normalize optional parameters when aclling taranis

--- taranis.py
+++ taranis.py
@@ -25,7 +25,7 @@ def check_arg (args=None) :
                                      formatter_class=argparse.RawDescriptionHelpFormatter,
                                      description= 'Taranis is a set of utilities related to cgMSLST ')
 
-    parser.add_argument('--version', action='version', version='%(prog)s 0.3.5')
+    parser.add_argument('-v, --version', action='version', version='%(prog)s 0.3.5')
 
     subparser = parser.add_subparsers(help = 'allele calling /interactive/schema '
                                       + 'are the available actions to execute taranis',
@@ -34,29 +34,30 @@ def check_arg (args=None) :
     ### Input parameters for allele calling option
     allele_calling_parser = subparser.add_parser('allele_calling',
                                     help = 'Allele calling way to downloads the  schema locus')
-    allele_calling_parser.add_argument('-coregenedir', required= True,
+    allele_calling_parser.add_argument('-c','--coregenedir', required= True,
                                     help = 'Directory where the core gene files are located ')
-    allele_calling_parser.add_argument('-inputdir', required= True,
+    allele_calling_parser.add_argument('-i', '--inputdir', required= True,
                                     help ='Directory where are located the sample fasta files')
-    allele_calling_parser.add_argument('-outputdir', required= True,
+    allele_calling_parser.add_argument('-o','--ouputdir', type=str, required= True,
                                     help = 'Directory where the result files will be stored')
     allele_calling_parser.add_argument('-cpus', required= False,
                                     help = 'Number of CPUS to be used in the program. Default is 1.',
                                     default = 1)
-    allele_calling_parser.add_argument('-updateschema' , required=False,
+    allele_calling_parser.add_argument('-u', '--updateschema' , required=False,
                                     help = 'Create a new schema with the new locus found. Default is True.',
                                     default = True)
+    '''
     allele_calling_parser.add_argument('-percentlength', required=False,
                                     help = 'Allowed length percentage to be considered as ASM or ALM. '
                                     + 'Outside of this limit it is considered as LNF Default is 20.',
                                     default = 20)
-
+    '''
     ### Input parameters for schema evaluation options
     evaluate_schema_parser = subparser.add_parser('evaluate_schema',
                                     help = 'Evaluate the schema.')
-    evaluate_schema_parser.add_argument('-inputdir',required= True,
+    evaluate_schema_parser.add_argument('-i ', '--inputdir',required= True,
                                     help = 'Directory where are the schema files.')
-    evaluate_schema_parser.add_argument('-outputdir', required= True,
+    evaluate_schema_parser.add_argument('-o', '--outputdir', required= True,
                                     help = 'Directory where the result files will be stored.')
     evaluate_schema_parser.add_argument('-alt', required = False, action = "store_true" ,
                                     help = 'Set this parameter if alternative start codon should be considered. '
@@ -74,7 +75,7 @@ def check_arg (args=None) :
     create_schema_parser = subparser.add_parser('create_schema', help = 'Create a schema.')
     create_schema_parser.add_argument('-xlsfile',
                                       help = 'xls file name which contains the list of the core genes.')
-    create_schema_parser.add_argument('-outputdir', help = 'Directory where the core gene files '
+    create_schema_parser.add_argument('-o', '--outputdir', help = 'Directory where the core gene files '
                                       + 'will be stored. If directory exists it will be prompt for '
                                       + 'deletion confirmation.')
 
@@ -97,7 +98,7 @@ def processing_create_schema (arguments) :
     return True
 
 if __name__ == '__main__' :
-    version = 'taranis version 0.2.2'
+    version = 'taranis version 0.3.5'
     if len(sys.argv) == 1 :
         print( 'Mandatory parameters are missing to execute the program. \n ' ,'Usage: "taranis.py  -help " for more information \n')
         exit (0)
@@ -113,14 +114,9 @@ if __name__ == '__main__' :
     elif arguments.chosen_action == 'create_schema' :
         result = processing_create_schema(arguments)
     else:
-        print('not allow')
-        result = 'Error'
-    '''
-    if 'Error' in result :
-        print('Exiting the code with errors. ')
-        print('Check the log for more information')
-    else:
-    '''
+        print('not allowed option. Exiting the program')
+        exit (0)
+
     print('completed')
 
 
