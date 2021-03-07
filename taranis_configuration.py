1ce3a498ec336fbb581d1c3db3566264bce1a246
espelpz <esperanzalpz96@gmail.com>
2021-03-07 13:43:13 +0100

    establising configuration

--- taranis_configuration.py
+++ taranis_configuration.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 ###  Settings configuration  for logging
-LOGGING_CONFIGURATION = '/srv/taranis/logging_config.ini'
+LOGGING_CONFIGURATION = 'Programas/taranis/logging_config.ini'
 
 
 ## Settings configuration for create schema functionality
