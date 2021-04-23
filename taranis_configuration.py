9c996f90518da97d6718ca0e6b505f29ab3b6c22
espelpz <esperanzalpz96@gmail.com>
2021-04-23 10:01:53 +0200

    Changing logging configuration to relative paths

--- taranis_configuration.py
+++ taranis_configuration.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
-###  Settings configuration  for logging
-LOGGING_CONFIGURATION = 'Programas/taranis/logging_config.ini'
 
+import os
+
+###  Settings configuration  for logging
+taranis_dir = os.path.dirname(os.path.realpath(__file__))
+###LOGGING_CONFIGURATION = taranis_dir
+###LOGGING_CONFIGURATION = taranis_dir + '/logging_config.ini'
+LOGGING_CONFIGURATION = os.path.join(taranis_dir,'logging_config.ini')
+print(LOGGING_CONFIGURATION)
 
 ## Settings configuration for create schema functionality
 
