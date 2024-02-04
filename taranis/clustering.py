e03691da607afbff50098f343ff041627b39e1a1
luissian <chapado.l@gmail.com>
2024-02-04 22:46:09 +0100

    formating files

--- taranis/clustering.py
+++ taranis/clustering.py
@@ -2,7 +2,6 @@ import igraph as ig
 import leidenalg
 import logging
 import numpy as np
-import pdb
 import rich.console
 
 import taranis.utils
@@ -16,27 +15,28 @@ stderr = rich.console.Console(
 )
 
 
-
 class ClusterDistance:
-    def __init__(self, dist_matrix:np.array, ref_seq_name: str):
+    def __init__(self, dist_matrix: np.array, ref_seq_name: str):
         self.dist_matrix = dist_matrix
         self.num_seq = dist_matrix.shape[0]
         self.ref_seq_name = ref_seq_name
         self.seed = None
         self.res_param = 0.9
 
-
     def calculate_mean_cluster(self, src_cluster_row_idxs):
-        
         src_cluster_col_idxs = src_cluster_row_idxs
         src_cluster_mtrx_idxs = np.ix_(src_cluster_row_idxs, src_cluster_col_idxs)
         row_idx_pos = np.argwhere(src_cluster_row_idxs).flatten()
         col_idx_pos = np.argwhere(src_cluster_col_idxs).flatten()
         num_of_diag_elements = np.intersect1d(row_idx_pos, col_idx_pos).size
-        num_of_non_diag_elements = row_idx_pos.size * col_idx_pos.size - num_of_diag_elements
+        num_of_non_diag_elements = (
+            row_idx_pos.size * col_idx_pos.size - num_of_diag_elements
+        )
         if num_of_non_diag_elements == 0:
             return 1
-        return (np.sum(self.dist_matrix[src_cluster_mtrx_idxs]) - num_of_diag_elements) / num_of_non_diag_elements
+        return (
+            np.sum(self.dist_matrix[src_cluster_mtrx_idxs]) - num_of_diag_elements
+        ) / num_of_non_diag_elements
 
     def convert_to_seq_clusters(seq_cluster_ptrs, seq_id_to_seq_name_map):
         output_seq_clusters = list()
@@ -44,36 +44,46 @@ class ClusterDistance:
         for cluster_id in range(np.max(seq_cluster_ptrs) + 1):
             seq_cluster = list()
             for seq_id in np.argwhere(seq_cluster_ptrs == cluster_id).flatten():
-                seq_cluster.append('{}{}'.format(seq_id_to_seq_name_map[str(seq_id)], os.linesep))
+                seq_cluster.append(
+                    "{}{}".format(seq_id_to_seq_name_map[str(seq_id)], os.linesep)
+                )
 
             output_seq_clusters.append(seq_cluster)
-        import pdb; pdb.set_trace()
         return output_seq_clusters
 
-    def verify_cluster_quality(self , src_cluster_ptrs):
+    def verify_cluster_quality(self, src_cluster_ptrs):
         log.debug(f"Verifying cluster for {self.ref_seq_name}")
         avg_clusters = {}
         for cluster_id in range(np.max(src_cluster_ptrs) + 1):
-                log.debug(f"calculating mean for cluster number {cluster_id}")
-                src_cluster_bool_ptrs = (src_cluster_ptrs == cluster_id)
-                avg_clusters[cluster_id] = self.calculate_mean_cluster(src_cluster_bool_ptrs)
+            log.debug(f"calculating mean for cluster number {cluster_id}")
+            src_cluster_bool_ptrs = src_cluster_ptrs == cluster_id
+            avg_clusters[cluster_id] = self.calculate_mean_cluster(
+                src_cluster_bool_ptrs
+            )
 
         return avg_clusters
 
-
-
     def create_clusters(self):
-        comm_graph = ig.Graph.Weighted_Adjacency(self.dist_matrix.tolist(), mode=1, loops=False)
-        graph_clusters = leidenalg.find_partition(comm_graph, leidenalg.CPMVertexPartition, weights='weight', n_iterations=-1, resolution_parameter=self.res_param, seed=self.seed)
+        comm_graph = ig.Graph.Weighted_Adjacency(
+            self.dist_matrix.tolist(), mode=1, loops=False
+        )
+        graph_clusters = leidenalg.find_partition(
+            comm_graph,
+            leidenalg.CPMVertexPartition,
+            weights="weight",
+            n_iterations=-1,
+            resolution_parameter=self.res_param,
+            seed=self.seed,
+        )
         cluster_ptrs = np.array(graph_clusters.membership)
-        pdb.set_trace()
 
         avg_clusters = self.verify_cluster_quality(cluster_ptrs)
         # check that cluste average values are upper than 0.9
-        pdb.set_trace()
         if not all(x > 0.9 for x in list(avg_clusters.values())):
-            log.warning(f"There are some cluster below average of 0.9 in locus {self.ref_seq_name} ")
-            stderr.print(f"[red]There are some cluster below average of 0.9 in locus {self.ref_seq_name}")
+            log.warning(
+                f"There are some cluster below average of 0.9 in locus {self.ref_seq_name} "
+            )
+            stderr.print(
+                f"[red]There are some cluster below average of 0.9 in locus {self.ref_seq_name}"
+            )
         return cluster_ptrs
-
-
--- taranis/reference_alleles.py
+++ taranis/reference_alleles.py
@@ -34,29 +34,32 @@ class ReferenceAlleles:
         self.output = output
         self.selected_locus = {}
 
-
     def create_cluster_alleles(self):
         log.debug("Processing distance matrix for $s", self.fasta_file)
         distance_obj = taranis.distance.DistanceMatrix(self.fasta_file)
         mash_distance_df = distance_obj.create_matrix()
         # fetch the allele position into array
-        postition_to_allele = {x: mash_distance_df.columns[x] for x in range(len(mash_distance_df.columns))}
+        postition_to_allele = {
+            x: mash_distance_df.columns[x] for x in range(len(mash_distance_df.columns))
+        }
         log.debug(f"Created distance matrix for {self.fasta_file}")
-        position_to_allele = {x: mash_distance_df.columns[x] }
+        position_to_allele = {x: mash_distance_df.columns[x]}
         # convert the  triangle matrix into full data matrix
         matrix_np = mash_distance_df.to_numpy()
         t_matrix_np = matrix_np.transpose()
         matrix_np = t_matrix_np + matrix_np
         # At this point minimal distance is 0. For clustering requires to be 1
         # the oposite.
-        dist_matrix_np = (matrix_np -1) * -1
+        dist_matrix_np = (matrix_np - 1) * -1
         """ in alfaclust TO DELETE
         sparse_edge_weight_mtrx = coo_matrix(global_edge_weight_mtrx, shape=global_edge_weight_mtrx.shape)
         """
         # create a sparse matrix used for summary
         sparse_edge_weight_mtrx = coo_matrix(matrix_np, shape=matrix_np.shape)
         pdb.set_trace()
-        cluster_seq = taranis.clustering.ClusterDistance(dist_matrix_np, self.locus_name)
+        cluster_seq = taranis.clustering.ClusterDistance(
+            dist_matrix_np, self.locus_name
+        )
         cluster_ptrs = cluster_seq.create_clusters()
         cluster_seq.convert_to_seq_clusters(cluster_ptrs, postition_to_allele)
         # f_name = os.path.basename(self.fasta_file).split('.')[0]
@@ -80,12 +83,9 @@ class ReferenceAlleles:
         # )
         # Get pairwise allele sequences mash distances
         # mash_distance_command = ["mash", "dist", sketch_path, sketch_path]
-       
-
-       
 
         pdb.set_trace()
-        
+
         # Create the blast distance. database is the locus and the query is each
         # allele in the locus file
         # Create  blast db with sample file
