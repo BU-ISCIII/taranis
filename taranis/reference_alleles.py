f57b881aaddfac24e380329d3517a5866f1cac22
luissian <chapado.l@gmail.com>
2024-02-07 21:11:58 +0100

    added documentation for each function

--- taranis/reference_alleles.py
+++ taranis/reference_alleles.py
@@ -4,7 +4,6 @@ import rich.console
 from pathlib import Path
 import os
 
-from scipy.sparse import coo_matrix
 import pdb
 import taranis.utils
 import taranis.distance
@@ -21,13 +20,31 @@ stderr = rich.console.Console(
 
 
 class ReferenceAlleles:
-    def __init__(self, fasta_file, output):
+    def __init__(self, fasta_file: str, output: str):
+        """ReferenceAlleles instance creation
+
+        Args:
+            fasta_file (str): file name included path for locus
+            output (str): output folder
+        """
         self.fasta_file = fasta_file
         self.locus_name = Path(fasta_file).stem
         self.output = output
         self.selected_locus = {}
 
-    def create_cluster_alleles(self):
+    def create_cluster_alleles(self) -> list:
+        """Alleles in fasta file are clustering by using two additional classes:
+            DistanceMatrix which creates a matrix of distance using the allele
+            sequences, and ClusterDistance which get the matrix and group the
+            alleles in clusters. As per result of ClusterDistance methods, the
+            reference alleles are saved to file and statistics information is
+            returned
+
+        Returns:
+            list: two dictionaires are returned, cluster_data having statistics
+                and reference_alleles, where keys are cluster number and value
+                the reference allele for the cluster
+        """
         log.debug("Processing distance matrix for $s", self.fasta_file)
         distance_obj = taranis.distance.DistanceMatrix(self.fasta_file)
         mash_distance_df = distance_obj.create_matrix()
@@ -44,9 +61,6 @@ class ReferenceAlleles:
         # the oposite.
         dist_matrix_np = (matrix_np - 1) * -1
 
-        # create a sparse matrix used for summary
-        _ = coo_matrix(matrix_np, shape=matrix_np.shape)
-
         cluster_obj = taranis.clustering.ClusterDistance(
             dist_matrix_np, self.locus_name
         )
@@ -61,19 +75,24 @@ class ReferenceAlleles:
         alleles_in_cluster = cluster_obj.convert_to_seq_clusters(
             cluster_ptrs, postition_to_allele
         )
+        cluster_folder = os.path.join(self.output, "Clusters")
+        _ = taranis.utils.create_new_folder(cluster_folder)
         cluster_file = os.path.join(
-            self.output, "cluster_alleles_" + self.locus_name + ".txt"
+            cluster_folder, "cluster_alleles_" + self.locus_name + ".txt"
         )
-        pdb.set_trace()
         with open(cluster_file, "w") as fo:
             for cluster_id, alleles in alleles_in_cluster.items():
                 fo.write("Cluster number" + str(cluster_id + 1) + "\n")
                 fo.write("\n".join(alleles) + "\n")
-        pdb.set_trace()
 
-        return cluster_data, reference_alleles
+        return [cluster_data, reference_alleles]
 
     def save_reference_alleles(self, reference_alleles: list) -> None:
+        """From the input list it fetch the allele squence and save it as fasta
+
+        Args:
+            reference_alleles (list): list having the allele ids
+        """
         record_seq = {}
         with open(self.fasta_file) as fh:
             for record in SeqIO.parse(fh, "fasta"):
@@ -86,10 +105,53 @@ class ReferenceAlleles:
                 fo.write(r_seq + "\n")
         return
 
-    def create_ref_alleles(self):
+    def create_ref_alleles(self) -> dict:
+        """Main method to create the reference alleles
+
+        Returns:
+            dict: statistics information for each cluster
+        """
         self.records = taranis.utils.read_fasta_file(self.fasta_file)
         # Prepare data to use mash to create the distance matrix
         cluster_data, reference_alleles = self.create_cluster_alleles()
         _ = self.save_reference_alleles(reference_alleles)
+        return cluster_data
 
-        pass
+
+def collect_statistics(data_alleles: list, out_folder: str) -> None:
+    """Collect the individual statistics for each locus to create graphics
+
+    Args:
+        data_alleles (list): list having the indiviual statistics data
+        out_folder (str): folder to save graphics
+    """
+
+    def stats_graphics(stats_folder: str, cluster_alleles: dict) -> None:
+        """Create the statistics graphics. Bar graphic for number of cluster
+            per alleles
+
+        Args:
+            stats_folder (str): folder path to store graphic
+            cluster_alleles (dict): contain number of clusters as key and number
+                of alleles having the same cluster number as value
+        """
+        stderr.print("Creating graphics")
+        log.info("Creating graphics")
+        graphic_folder = os.path.join(stats_folder, "graphics")
+        _ = taranis.utils.create_new_folder(graphic_folder)
+        cluster, alleles = zip(*cluster_alleles.items())
+        _ = taranis.utils.create_graphic(
+            graphic_folder,
+            "num_genes_per_allele.png",
+            "bar",
+            cluster,
+            alleles,
+            ["Number of clusters", "number of genes"],
+            "Number of cluster per gene",
+        )
+
+    cluster_alleles = {}
+    for d_allele in data_alleles:
+        cluster_alleles[len(d_allele)] = cluster_alleles.get(len(d_allele), 0) + 1
+    _ = stats_graphics(out_folder, cluster_alleles)
+    return
